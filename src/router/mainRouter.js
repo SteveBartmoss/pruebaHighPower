e5cae5804f1c46021f9eeced6b5ccda6bc99a9ec
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-08-07 21:50:34 -0600

    feat: Se agrego una ruta para el bonus

--- src/router/mainRouter.js
+++ src/router/mainRouter.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../components/Home.vue";
+import Pokedex from "../components/Pokedex.vue";
 
 
 const routes = [
-    {path: '/', name: 'Home', component: Home}
+    {path: '/', name: 'Home', component: Home},
+    {path: '/bonus', name: 'Bonus', component: Pokedex}
 ]
 
 const router = createRouter({
