a7f55b205a8cb7c086a9fb940a9c425454365911
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-08-07 18:05:33 -0600

    feat: se configuro el main app para la utilizar la libreria vuetify

--- src/main.js
+++ src/main.js
@@ -2,11 +2,22 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import router from './router/mainRouter'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
 const app = createApp(App)
 
 const pinia = createPinia()
-app.use(pinia)
 
+app.use(vuetify)
+app.use(pinia)
+app.use(router)
 app.mount('#app')
